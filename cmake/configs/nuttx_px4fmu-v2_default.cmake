fc7693225a09e88548007f8c648649b79f5e37a1
Nicolas de Palezieux <ndepal@gmail.com>
2017-02-23 14:38:46 +0100

    precland: refactor beacon estimator

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -116,7 +116,7 @@ set(config_module_list
 	modules/ekf2
 	#modules/local_position_estimator
 	#modules/position_estimator_inav
-	modules/precland_beacon_est
+	modules/beacon_position_estimator
 
 	#
 	# Vehicle Control

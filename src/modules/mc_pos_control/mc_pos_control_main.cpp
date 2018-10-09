b4072de5ed2fc14ce36b1a4a02305809b20b66b2
Mohammed Kabir <kabir@uasys.io>
2018-10-08 23:42:06 -0400

    mc_pos_control : allow obstacle avoidance in position control mode

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -1104,7 +1104,8 @@ MulticopterPositionControl::use_obstacle_avoidance()
 	return (MPC_OBS_AVOID.get()
 		&& (hrt_elapsed_time((hrt_abstime *)&_traj_wp_avoidance.timestamp) < TRAJECTORY_STREAM_TIMEOUT_US)
 		&& (_traj_wp_avoidance.waypoints[vehicle_trajectory_waypoint_s::POINT_0].point_valid == true)
-		&& ((_vehicle_status.nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION) ||
+		&& ((_vehicle_status.nav_state == vehicle_status_s::NAVIGATION_STATE_POSCTL) ||
+			(_vehicle_status.nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION) ||
 		    (_vehicle_status.nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_RTL)));
 }
 

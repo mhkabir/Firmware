6daf34b300ef19268830491e1f1ff79dfdc8b667
Mohammed Kabir <kabir@uasys.io>
2019-03-28 23:09:11 -0400

    msg : Add wheel odometry message

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -660,6 +660,7 @@ void Logger::add_default_topics()
 	add_topic("vehicle_status_flags");
 	add_topic("vtol_vehicle_status", 200);
 	add_topic("wind_estimate", 200);
+	add_topic("wheel_odometry");
 
 #ifdef CONFIG_ARCH_BOARD_PX4_SITL
 	add_topic("actuator_controls_virtual_fw");
